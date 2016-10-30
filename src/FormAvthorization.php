d699a716ea71942412a8271991be1a8a2a899f86
Vladimir Nimchyn <nima.vova@gmail.com>
2016-10-30 15:55:15 +0200

    add files classes moduls

---
+++ src/FormAvthorization.php
@@ -0,0 +1,42 @@
+<?php
+
+class FormInputInterbase implements FormInputInterbase
+{
+    /**
+     * @var string
+     */
+    public
+        $email;
+
+    /**
+     * @var string
+     */
+    public
+        $login;
+
+    /**
+     * @var string
+     */
+    public
+        $passw;
+
+    public function emailValidator()
+    {
+
+        /**
+         * @param $var
+         */
+    }
+
+    public function emailValidator()
+    }
+
+    /**
+    * @param $var
+    */
+    {
+
+
+
+
+}
---
+++ src/FormInputInterbase.php
@@ -0,0 +1,17 @@
+<?php
+
+   namespace Vendor\Moduls;
+
+   interface FormInputInterbase
+{
+/**
+* @param $var
+*/
+   public function emailValidator();
+
+
+      /**
+       * @param $var
+       */
+   public function emailValidator();
+}
